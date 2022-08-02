49d755e7d814a8162208b3e076bbb3f18a84108f
Ksenia S <iksilfb@gmail.com>
2022-08-02 15:36:27 +0300

    Function homework commit

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,31 +1,76 @@
 // Задание 1
 function getArrayParams(arr) {
-  let min, max, sum, avg;
+  let min =arr[0], 
+      max = min, 
+      sum = 0, 
+      avg = 0;
 
   // Ваш код
+  for (let i = 0; i < arr.length; i++ ) {
+    if (arr[i] > max) max = arr[i];
+    if (arr[i] < min) min = arr[i];
+    sum += arr[i];
+  }
+
+   avgCount = +(sum/arr.length);
+   avgCount.toFixed(2);
+   avg = Number(avgCount);
 
   return { min: min, max: max, avg: avg };
 }
+console.log( getArrayParams([-99, 99, 10]));
+
 
 // Задание 2
+
 function worker(arr) {
-  let sum;
+  let sum = 0;
 
   // Ваш код
+  for ( let i = 0; i < arr.length; i++ ) {
+    sum += arr[i];
+  }
 
   return sum;
 }
+//console.log(worker([4, 5, 6]))
+
 
 function makeWork(arrOfArr, func) {
-  let max;
+  let max = func(arrOfArr[0]);
 
   // Ваш кода
-  // for ...
-  
+  //for ...
+  for (let i = 0; i < arrOfArr.length; i++){
+    if(func(arrOfArr[i] > max)){
+      max = func(arrOfArr[i]);
+    }
+  }
   return max;
 }
+//let arrOfArr = [[1, 2, 3], [4, 5, 6]];
+//console.log(makeWork(arrOfArr, worker))
+
+
 
 // Задание 3
 function worker2(arr) {
-  // Ваш код
+  // Ваш код 
+  let min =arr[0]; 
+  let max = min;
+
+// Ваш код
+for (let i = 0; i < arr.length; i++ ) {
+  if (arr[i] > max) max = arr[i];
+  if (arr[i] < min) min = arr[i];
 }
+
+let diff = max - min;
+return Math.abs(diff);
+}
+
+console.log(worker2([-10, -20, -40]));
+console.log(worker2([10, 20, 30]));
+
+let arrOfArr = [[-10, -20, -40], [10, 20, 30]];
+console.log(makeWork(arrOfArr, worker2));
