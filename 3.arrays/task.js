d2473813278689a42c8ed1e6c64a29ae099c8b5d
Ksenia S <iksilfb@gmail.com>
2022-08-09 17:35:21 +0300

    Arrays HomeWork

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,16 @@
 function compareArrays(arr1, arr2) {
-  let result;
+  let result = arr1.length === arr2.length && arr1.every((val, i) => val === arr2[i]);
+  return result; 
+ }
+console.log(compareArrays([2, 2, 2], [2, 2, 2]));
+console.log(compareArrays([8, 9], [6]));
+console.log(compareArrays([8, 9, 5, 4], [8, 9, 5, 4, 8, 3, 5]));
 
-  // Ваш код
-
-  return result; // boolean
-}
 
 function advancedFilter(arr) {
-  let resultArr;
+  //let resultArr;
 
   // Ваш код
-
+  let resultArr = arr.filter((n) => n >=0).filter((n)=> n % 3 === 0).map(n => n * 10);
   return resultArr; // array
 }
