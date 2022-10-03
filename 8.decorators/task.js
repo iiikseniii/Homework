db7f419ace2a6afa3e4f63ede6ccc412fc2d7c91
Ksenia S <iksilfb@gmail.com>
2022-10-03 22:16:46 +0300

    Decorators hw1

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,12 +1,58 @@
 function cachingDecoratorNew(func) {
-  // Ваш код
+  let cache = [];
+  function wrapper(...args){
+    const hash = args.join(',');
+    let objectInCache = cache.find((item) => item['hash'] === hash);
+    if(objectInCache){
+      console.log( "Из кэша: " + objectInCache['value']);
+      return "Из кэша: " +  objectInCache['value'];
+    }
+
+    let result = func(...args); 
+    cache.push({'hash': hash, 'value': result }) ; 
+    if (cache.length > 5) { 
+      cache.shift(); 
+    }
+    console.log("Вычисляем: " + result);
+    return "Вычисляем: " + result;  
+  }
+  return wrapper;
 }
 
 
-function debounceDecoratorNew(func) {
-  // Ваш код
-}
+const debounceDecoratorNew = (f, ms) => {
+  let timerId;
+  let firstRun = true;
 
-function debounceDecorator2(func) {
-  // Ваш код
+  return function(...args){
+    if(firstRun){
+      timerId = setTimeout(() => {
+        f.apply(this, args)}, 0);
+      firstRun = false;
+    } else {
+      clearTimeout(timerId);
+      timerId = setTimeout(() => {
+        f.apply(this, args)}, ms);
+    }
+  };
+};
+
+const debounceDecorator2 = (func, ms) => {
+  let timerId;
+  let firstRun = true;
+  wrapper.count = 0;
+
+  function wrapper(...args){
+    if(firstRun){
+      timerId = setTimeout(() => {
+        f.apply(this, args)}, 0);
+      firstRun = false;
+    } else {
+      clearTimeout(timerId);
+      timerId = setTimeout(() => {
+        f.apply(this, args)}, ms);
+    }
+    this.count ++;
+  }
+  return wrapper;
 }
